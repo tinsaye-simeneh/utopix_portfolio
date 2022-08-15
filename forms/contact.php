b19c6c7c1a231ab06a23cb308ee30e4a89bdeea3
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-08-15 21:56:35 +0300

    contact.php update

--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,4 @@
 <?php
-  // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'contact@example.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
